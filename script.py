86a3d20af1c6a9b6366ea5e73dba69ad45b0c51d
Maxim <maxtinion@gmail.com>
2023-12-31 09:36:12 -0700

    Updated to use tmailor, higher chance of not getting stopped by seed4.me. a lot was implemented some may be taken out.

--- script.py
+++ script.py
@@ -1,10 +1,26 @@
+import sys
+import time
+
+import pyperclip
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def create_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=512,512")
+
+    # Use ChromeDriverManager to automatically download and manage the ChromeDriver executable
+    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+
+    return browser
 
 
 def wait(seed):
     seed.implicitly_wait(30)
+
 
 def generate_password(n):
     import random
@@ -14,30 +30,57 @@ def generate_password(n):
         password += random.choice(string.ascii_letters + string.digits + string.punctuation + string.digits)
     return password
 
-chrome_options = Options()
-chrome_options.add_argument("--incognito")
 
-mohml = webdriver.Chrome(chrome_options=chrome_options)
-mohml.get("https://www.mohmal.com/")
-wait(mohml)
-create_account = mohml.find_element(By.XPATH, "//*[@id=\"rand\"]")
-create_account.click()
-wait(mohml)
+def write_credentials(username, password):
+    with open('credentials.txt', 'w') as f:
+        f.write(f"Username: {username}\nPassword: {password}")
+        print("Credentials written to file")
 
-email_element = mohml.find_element(By.XPATH, "//*[@id=\"email\"]/div[1]")
-email = email_element.text
 
-seed = webdriver.Chrome(chrome_options=chrome_options)
+def handle_error_block(driver):
+    try:
+        # Try to locate the error block element
+        error_block = driver.find_element(By.XPATH, "/html/body/div[3]/div/div")
+
+        # If NoSuchElementException is not raised, the error block is present
+        print(error_block.text)
+        sys.exit()
+
+    except NoSuchElementException:
+        # If NoSuchElementException is raised, the error block is not present
+        print("Account created successfully, you have 50 seconds to verify your email address")
+        time.sleep(50)
+        sys.exit()
+
+
+tmailor = create_browser()
+tmailor.get("https://tmailor.com/en")
+wait(tmailor)
+time.sleep(2)
+print("Copied email button clicked")
+copy_email = tmailor.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div/div[1]/div[1]/div[3]/button[1]')
+copy_email.click()
+wait(tmailor)
+
+email = pyperclip.paste()
+time.sleep(2)
+
+seed = create_browser()
 seed.get("https://seed4.me/")
 wait(seed)
+print("Navigated to seed4.me")
 
+threeBars = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/a[1]")
+threeBars.click()
 my_account = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
 my_account.click()
 wait(seed)
+print("Clicked on My Account")
 
 create_seed4me_account = seed.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
 create_seed4me_account.click()
 wait(seed)
+print("Clicked on Create Seed4.me Account")
 
 email_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
 email_textbox.send_keys(email)
@@ -48,9 +91,18 @@ confirm_password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserConfirmPas
 confirm_password_textbox.send_keys(password)
 confirm = seed.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
 confirm.click()
+print("Filled in registration details")
 
 register = seed.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
 register.click()
+print("Clicked on Register")
+
+write_credentials(email, password)
+print("Credentials written to file")
+seed.close()
+time.sleep(45)
+#handle_error_block(seed)
+sys.exit()
 
 while True:
     pass
