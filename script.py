e9f4bb9dbab01866da349b0989b779d25405cc5d
Maxim <maxtinion@gmail.com>
2024-02-22 07:12:24 -0700

    finally fixed deprecation warning, also finished error block detection so now the program lets you know it didn't work and closes itself!

--- script.py
+++ script.py
@@ -1,33 +1,35 @@
 import sys
 import time
-
+import random
+import string
 import pyperclip
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Constants
+URL_TMAILOR = "https://tmailor.com/en"
+URL_SEED = "https://seed4.me/"
+WAIT_TIME = 30
+PASSWORD_LENGTH = 10
 
 
 def create_browser():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument("--window-size=512,512")
-
-    # Use ChromeDriverManager to automatically download and manage the ChromeDriver executable
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return browser
 
 
-def wait(seed):
-    seed.implicitly_wait(30)
+def wait(browser):
+    browser.implicitly_wait(WAIT_TIME)
 
 
 def generate_password(n):
-    import random
-    import string
-    password = ''
-    for i in range(n):
-        password += random.choice(string.ascii_letters + string.digits + string.punctuation + string.digits)
+    password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(n))
     return password
 
 
@@ -39,69 +41,53 @@ def write_credentials(username, password):
 
 def handle_error_block(driver):
     try:
-        # Try to locate the error block element
-        error_block = driver.find_element(By.XPATH, "/html/body/div[3]/div/div")
-
-        # If NoSuchElementException is not raised, the error block is present
+        error_block = driver.find_element(By.CSS_SELECTOR, ".alert.alert-block.alert-error.fade.in.out")
         print(error_block.text)
         sys.exit()
-
     except NoSuchElementException:
-        # If NoSuchElementException is raised, the error block is not present
         print("Account created successfully, you have 50 seconds to verify your email address")
         time.sleep(50)
         sys.exit()
 
 
-tmailor = create_browser()
-tmailor.get("https://tmailor.com/en")
-wait(tmailor)
-time.sleep(2)
-print("Copied email button clicked")
-copy_email = tmailor.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div/div[1]/div[1]/div[3]/button[1]')
-copy_email.click()
-wait(tmailor)
+def main():
+    tmailor = create_browser()
+    tmailor.get(URL_TMAILOR)
+    wait(tmailor)
+    time.sleep(2)
+    copy_email = tmailor.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div/div[1]/div[1]/div[3]/button[1]')
+    copy_email.click()
+    print("Email copied to clipboard")
+    wait(tmailor)
+    email = pyperclip.paste()
+    time.sleep(2)
+    seed = create_browser()
+    seed.get(URL_SEED)
+    wait(seed)
+    threebars = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/a[1]")
+    threebars.click()
+    my_account = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
+    my_account.click()
+    wait(seed)
+    create_seed4me_account = seed.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
+    create_seed4me_account.click()
+    wait(seed)
+    email_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
+    email_textbox.send_keys(email)
+    password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserPassword\"]")
+    password = generate_password(PASSWORD_LENGTH)
+    password_textbox.send_keys(password)
+    confirm_password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserConfirmPassword\"]")
+    confirm_password_textbox.send_keys(password)
+    confirm = seed.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
+    confirm.click()
+    register = seed.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
+    register.click()
+    write_credentials(email, password)
+    handle_error_block(seed)
+    seed.close()
+    sys.exit()
 
-email = pyperclip.paste()
-time.sleep(2)
 
-seed = create_browser()
-seed.get("https://seed4.me/")
-wait(seed)
-print("Navigated to seed4.me")
-
-threeBars = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/a[1]")
-threeBars.click()
-my_account = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
-my_account.click()
-wait(seed)
-print("Clicked on My Account")
-
-create_seed4me_account = seed.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
-create_seed4me_account.click()
-wait(seed)
-print("Clicked on Create Seed4.me Account")
-
-email_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
-email_textbox.send_keys(email)
-password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserPassword\"]")
-password = generate_password(10)
-password_textbox.send_keys(password)
-confirm_password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserConfirmPassword\"]")
-confirm_password_textbox.send_keys(password)
-confirm = seed.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
-confirm.click()
-print("Filled in registration details")
-
-register = seed.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
-register.click()
-print("Clicked on Register")
-
-write_credentials(email, password)
-seed.close()
-time.sleep(45)
-#handle_error_block(seed)
-sys.exit()
-
-while True:
-    pass
+if __name__ == "__main__":
+    main()
